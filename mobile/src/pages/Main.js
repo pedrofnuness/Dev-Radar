b1a9dbbb5f3f17926b03187823c376ec70834499
Pedro Nunes <pedro9661@gmail.com>
2020-01-28 17:47:44 -0200

    Added search input

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native'
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native'
 import MapView, { Marker, Callout } from 'react-native-maps';
-import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from "@expo/vector-icons";
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -34,21 +35,37 @@ function Main({ navigation }) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -30.0597843, longitude: -51.1738167 }}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/49870721?s=460&v=4' }}/>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -30.0597843, longitude: -51.1738167 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/49870721?s=460&v=4' }}/>
 
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'pedrofnuness' });
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Pedro Nunes</Text>
-                        <Text style={styles.devBio}>Dev. Front-End no inicio da sua carreira e sempre em busca de aprendizado.</Text>
-                        <Text style={styles.devTechs}>ReactJS, Angular, JavaScript</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'pedrofnuness' });
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Pedro Nunes</Text>
+                            <Text style={styles.devBio}>Dev. Front-End no inicio da sua carreira e sempre em busca de aprendizado.</Text>
+                            <Text style={styles.devTechs}>ReactJS, Angular, JavaScript</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+
+            <View style={styles.searchForm}>
+                <TextInput 
+                    style={styles.searchInput} 
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color='#FFF' />
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -81,6 +98,42 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5
+    },
+
+    searchForm: {
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 5,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4Dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15
     }
 })
 
